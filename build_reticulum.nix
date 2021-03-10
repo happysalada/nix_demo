89d53519320d1b5a125d06c3c8db651ef574a8cb
happysalada <raphael@megzari.com>
2021-03-10 11:30:11 +0900

    update build_reticulum

--- build_reticulum.nix
+++ build_reticulum.nix
@@ -6,23 +6,19 @@ let
     rev = "5f216106aef34a1953a5e9b90f09ee096f4d5e01";
   };
 
-  name = "reticulum";
+  pname = "reticulum";
   version = "0.0.1";
   mixEnv = "prod";
 
   mixDeps = packages.fetchMixDeps {
-    inherit src name mixEnv version;
-    sha256 = "sha256-EmVS+vry0cg1N/ODLtJ27t/OnJr2Ock3B9PapGempLA=";
-    DATABASE_URL = "";
-    SECRET_KEY_BASE = "";
+    pname = "mix-deps-${pname}";
+    inherit src mixEnv version;
+    sha256 = "sha256-cfs1i5nCCnyhi82SFBO3m65pu4pxh6O9qd9VwoelnQI=";
   };
-
 
 in
 packages.buildMix {
-  inherit src name version mixEnv mixDeps;
-  DATABASE_URL = "";
-  SECRET_KEY_BASE = "";
+  inherit src pname version mixEnv mixDeps;
   defaultEnvVars = false;
   nativeBuildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.CoreServices ];
 }
